d311a0fbe2645648b1e25510749de4491040a5cf
LloydMontgomeryGarmadon <crazymathew29@gmail.com>
2023-07-22 14:23:21 +0200

    path

--- setup.py
+++ setup.py
@@ -163,8 +163,8 @@ kwargs = dict(
     long_description_content_type="text/markdown",
     zip_safe=False,
     project_urls={
-        "Source": "https://github.com/serwisgalena/cryptomines-blockchain/",
-        "Changelog": "https://github.com/serwisgalena/cryptomines-blockchain/blob/main/CHANGELOG.md",
+        "Source": "https://github.com/Cryptomines-Network/cryptomines-blockchain/",
+        "Changelog": "https://github.com/Cryptomines-Network/cryptomines-blockchain/blob/main/CHANGELOG.md",
     },
 )
 
